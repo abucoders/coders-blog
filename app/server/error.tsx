e36850fc8af124bcb0758d1a8c70dc5b16b181b1
abdulloyev <abdulloyev.dev@gmail.com>
2025-08-07 00:25:31 +0500

    error.tsx fail map

---
+++ app/server/error.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+const Error = () => {
+  return (
+    <div className=" w-full h-screen flex justify-center items-center">
+      <div className="w-1/3 bg-gray-800 p-8 rounded-2xl">
+        <h1 className="font-mono">Error</h1>
+      </div>
+    </div>
+  );
+};
+
+export default Error;
