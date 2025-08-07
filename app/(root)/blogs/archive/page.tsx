810f16658c2bf245ddf8e6b8d5e9cb6c79580dde
abdulloyev <abdulloyev.dev@gmail.com>
2025-08-07 23:16:46 +0500

    archive-page ui

---
+++ app/(root)/blogs/archive/page.tsx
@@ -0,0 +1,82 @@
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+  BreadcrumbPage,
+  BreadcrumbSeparator,
+} from "@/components/ui/breadcrumb";
+import { Archive, Dot, FileCode2, Home } from "lucide-react";
+import Link from "next/link";
+
+const ArchivePage = () => {
+  return (
+    <div className="max-w-6xl mx-auto">
+      <div className="relative min-h-[40vh] flex items-center justify-end flex-col">
+        <p className="text-lg text-muted-foreground">Showing posts from</p>
+        <h2 className="text-center text-4xl section-title font-creteRound mt-2">
+          <span>Archive</span>
+        </h2>
+
+        <div className="mt-4">
+          <Breadcrumb>
+            <BreadcrumbList>
+              <BreadcrumbItem>
+                <BreadcrumbLink asChild>
+                  <Link href="/" className="flex items-center gap-1">
+                    <Home className="size-4" /> Home
+                  </Link>
+                </BreadcrumbLink>
+              </BreadcrumbItem>
+
+              <BreadcrumbSeparator>
+                <Dot className="size-5" />
+              </BreadcrumbSeparator>
+
+              <BreadcrumbItem>
+                <BreadcrumbLink asChild>
+                  <Link href="/blogs" className="flex items-center gap-1">
+                    <FileCode2 className="size-4" /> Blogs
+                  </Link>
+                </BreadcrumbLink>
+              </BreadcrumbItem>
+
+              <BreadcrumbSeparator>
+                <Dot className="size-5" />
+              </BreadcrumbSeparator>
+
+              <BreadcrumbItem>
+                <BreadcrumbPage>Archive</BreadcrumbPage>
+              </BreadcrumbItem>
+            </BreadcrumbList>
+          </Breadcrumb>
+        </div>
+      </div>
+      <div className="flex flex-col space-y-3 mt-8">
+        <div className="relative">
+          <span className="text-5xl font-creteRound relative z-20">2025</span>
+          <Archive className="absolute w-16 h-16 -translate-x-4 -translate-y-12 opacity-10" />
+        </div>
+      </div>
+
+      <div className="flex flex-col space-y-2 mt-8">
+        <div className="flex gap-2 text-lg text-muted-foreground">
+          <p>05 Dec</p>
+          <Dot className="text-foreground w-8 h-8" />
+          <div className="hover:text-foreground hover:underline cursor-pointer">
+            The AGI hype train is running out of steam
+          </div>
+        </div>
+        <div className="flex gap-2 text-lg text-muted-foreground">
+          <p>05 Dec</p>
+          <Dot className="text-foreground w-8 h-8" />
+          <div className="hover:text-foreground hover:underline cursor-pointer">
+            The AGI hype train is running out of steam
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ArchivePage;
