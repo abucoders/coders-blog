e1558dd96f7e9f2c7c410c8e5180c29700fcb003
abdulloyev <abdulloyev.dev@gmail.com>
2025-08-09 00:02:26 +0500

    getReadingTime funck

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,30 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
 }
+
+export const getReadingTime = (content: string) => {
+  const WPS = 275 / 60; // Words per second
+
+  let images = 0;
+  const regex = /\w/;
+
+  const words = content.split(" ").filter(word => {
+    if (word.includes("<img")) {
+      images += 1;
+    }
+    return regex.test(word);
+  }).length;
+
+  const readingTimeSeconds = words / WPS;
+  const imageSeconds = images * 10;
+  const totalTimeSeconds = Math.ceil(readingTimeSeconds + imageSeconds);
+  const minutes = Math.ceil(totalTimeSeconds / 60);
+
+  return {
+    minutes,
+    text: `${minutes} min read`,
+  };
+};
