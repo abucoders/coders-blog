2d2546e02229b119544c3ad8b3b616252326564f
abdulloyev <abdulloyev.dev@gmail.com>
2025-08-10 16:25:28 +0500

    nextjs-toploader

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,11 +1,14 @@
 import { ChildProps } from "@/types";
 import { Footer, Navbar } from "./_components";
+import NextTopLoader from "nextjs-toploader";
 
 const Layout = ({ children }: ChildProps) => {
   return (
     <main>
+      <NextTopLoader />
+
       <Navbar />
-      <div >{children}</div>
+      <div>{children}</div>
       <Footer />
     </main>
   );
