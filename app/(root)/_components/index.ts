3d1431fca619d5aeec9a76cf588c5efc751713ec
abdulloyev <abdulloyev.dev@gmail.com>
2025-08-10 16:28:02 +0500

    share-btns component

--- app/(root)/_components/index.ts
+++ app/(root)/_components/index.ts
@@ -4,3 +4,4 @@ export { default as Footer } from "./footer";
 export { default as GlobalSearch } from "./global-search";
 export { default as GlobalBadge } from "./global-badge";
 export { default as Mobile } from "./mobile";
+export { default as ShareBtns } from "./share-btns";
